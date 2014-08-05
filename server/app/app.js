3a9e6fa54dc726fa60608bf1984f836b89817500
sahil <sahil.kabra@gmail.com>
2014-08-05 17:52:43 +0530

    Integration with simple messenger

--- server/app/app.js
+++ server/app/app.js
@@ -8,12 +8,12 @@ var morgan = require('morgan');
 var router = require('routes/router');
 var config = require('app/config');
 var app = express();
-var appEvent = require('app/pubSub');
+var messenger = require('messenger');
 
 app.use(morgan('short'));
 app.use(router);
 //app.use(error);
-appEvent.on('plan.dbConnected', function() {
+messenger.on('plan.dbConnected', function() {
         app.listen(config.express.port);
         console.log('Service (%d) started on port %d', config.pid, config.express.port);
 });
--- server/app/floorPlan/building/buildingModel.js
+++ server/app/floorPlan/building/buildingModel.js
@@ -5,11 +5,11 @@
  */
 var mongoClient = require('mongodb').MongoClient;
 var config = require('app/config').mongo;
-var appEvent = require('app/pubSub');
+var messenger = require('messenger');
 
 var db; 
 mongoClient.connect(config.url, function(err, database) {
         if (err) throw err;
         db = database;
-        appEvent.emit('plan.dbConnected');
+        messenger.emit('plan.dbConnected');
 });
