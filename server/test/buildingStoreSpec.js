cd233e45e583a9b91d40598c266838f567b5fc7e
sahil <sahil.kabra@gmail.com>
2014-08-06 17:46:15 +0530

    Moving to mocha/sinon/chai

--- server/test/buildingStoreSpec.js
+++ server/test/buildingStoreSpec.js
@@ -1,13 +1,15 @@
+var expect = require('chai').expect;
+
 describe('The building store', function() {
         var buildingStore = require('../app/floorPlan/building/buildingStore.js');
         it('returns a list of buildings', function() {
                 var buildings = buildingStore.get();
-                expect(buildings).toBeDefined();
+                expect(buildings).to.exist;
         });
         describe('passing criteria', function() {
                 it('returns a building when name is passed', function() {
                         var buildings = buildingStore.get({'name': 'T1'});
-                        expect(buildings).toBeDefined();
+                        expect(buildings).to.exist;
                         expect(buildings.length).toEqual(1);
                         expect(buildings[0].name).toEqual('T1');
                 });
--- server/test/floorPlanControllerSpec.js
+++ server/test/floorPlanControllerSpec.js
@@ -1,48 +1,66 @@
 var injectr = require('injectr');
 var mockBuildings = require('./mockData/buildings').mock;
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+chai.use(require('sinon-chai'));
 
 describe('The floor Plan Module', function() {
-        var mockBuildingStore = jasmine.createSpyObj('buildingStore', ['get']);
-        var mockFloorStore = jasmine.createSpyObj('floorStore', ['get']);
+        var mockStore = {get: function() { } };
+        var getSpy;
         var floorPlan = injectr('../app/floorPlan/floorPlanController.js', {
-                        './building/buildingStore': mockBuildingStore,
-                        './floor/floorStore': mockFloorStore
+                        './building/buildingStore': mockStore,
+                        './floor/floorStore': mockStore
+        });
+        beforeEach(function() {
+                getSpy = sinon.stub(mockStore, 'get');
+        });
+        afterEach(function() {
+                getSpy.restore();
         });
         it('returns a list of buildings', function() {
-                mockBuildingStore.get.andReturn(mockBuildings);
+                getSpy.returns(mockBuildings);
+
                 var buildings = floorPlan.getBuildings();
-                expect(mockBuildingStore.get).toHaveBeenCalled();
-                expect(buildings).toEqual(mockBuildings);
+
+                expect(getSpy).to.have.been.called;
+                expect(buildings).to.be.defined;
+                expect(buildings).to.equal(mockBuildings);
         });
         it('returns a building when a name is passed', function() {
-                mockBuildingStore.get.andReturn([mockBuildings[0]]);
+                getSpy.returns([mockBuildings[0]]);
+
                 var buildings = floorPlan.getBuildings({name: 'T1'});
-                expect(mockBuildingStore.get).toHaveBeenCalledWith({name: 'T1'});
-                expect(buildings).toBeDefined();
-                expect(buildings[0].name).toEqual('T1');
+
+                expect(getSpy).to.have.been.calledWith({name: 'T1'});
+                expect(buildings).to.be.defined;
+                expect(buildings[0].name).to.equal('T1');
         });
         it('returns a list of floors when a building name is passed', function() {
-                mockFloorStore.get.andReturn(mockBuildings[0].floors);
+                getSpy.returns(mockBuildings[0].floors);
+
                 var floors = floorPlan.getFloors({buildingName: 'T1'});
-                expect(mockFloorStore.get).toHaveBeenCalledWith({buildingName: 'T1'});
-                expect(floors).toBeDefined();
-                expect(floors[0].name).toEqual('N1');
-                expect(floors[0]['seating-capacity']).toEqual(10);
+
+                expect(getSpy).to.have.been.calledWith({buildingName: 'T1'});
+
+                expect(floors).to.be.defined;
+                expect(floors[0].name).to.equal('N1');
+                expect(floors[0]['seating-capacity']).to.equal(10);
         });
         it('returns a floor when a building name and floor name is passed', function() {
                 //Expecting get to be called
                 //Returning the second floor of the second building
-                mockFloorStore.get.andReturn([mockBuildings[1].floors[1]]);
+                getSpy.returns([mockBuildings[1].floors[1]])
 
                 var floors = floorPlan.getFloors({
                         buildingName: 'T2', 
                         floorName: 'N2'});
-                expect(floors).toBeDefined();
-                expect(mockFloorStore.get).toHaveBeenCalledWith({
+                expect(floors).to.be.defined;
+                expect(getSpy).to.have.been.calledWith({
                         buildingName: 'T2', 
                         floorName: 'N2'});
-                expect(floors).toBeDefined();
-                expect(floors[0].name).toEqual('N2');
-                expect(floors[0]['seating-capacity']).toEqual(20);
+                expect(floors).to.be.defined;
+                expect(floors[0].name).to.equal('N2');
+                expect(floors[0]['seating-capacity']).to.equal(20);
         });
 });
